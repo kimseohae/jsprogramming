86404824ae943d8982a6ca08be745d3c1333144f
lee <kim0lil@naver.com>
2017-11-06 18:07:52 +0900

    17/11/06

--- 러닝자바스크립트/smp.js
+++ 러닝자바스크립트/smp.js
@@ -1,6 +1,10 @@
-var obj = {
-
+// m 이상 n 이하의 무작위 정수를 반환합니다.
+function rand(m, n){
+    return m + Math.floor((n - m + 1)*Math.random());
 }
-obj.console
 
-console.log(obj)
+// 크라운 앤 앵커 게임의 여섯 가지 도형 중 하나를 무작위 반환합니다.
+function randFace() {
+    return ["crown","anchor","heart","spade","club","diamond"]
+        [rand(0,5)];
+}
